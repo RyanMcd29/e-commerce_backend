3f259dc94a56ca34f2b810b48ef9268ebf5abd47
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-19 21:53:56 +0800

    inputed ownership into index

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Categories.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  through: 'ProductTag'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: 'ProductTag'
+})
 
 module.exports = {
   Product,
