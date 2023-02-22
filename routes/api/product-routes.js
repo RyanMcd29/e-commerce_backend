06080a8f5d089d9bbb0239806c9cef25ef621981
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-22 20:00:29 +0800

    Get product by ID

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,8 +21,22 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, {model: Tag}],
+    })
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this ID.'})
+      return
+    }
+    res.status(200).json(productData)
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  });
   // be sure to include its associated Category and Tag data
-});
 
 // create new product
 router.post('/', (req, res) => {
