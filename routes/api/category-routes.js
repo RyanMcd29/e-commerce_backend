ee3517d3b5f391cff462bfab21a1b1887653c8be
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-22 19:55:30 +0800

    get all products route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
       include: [{model: Product}],
     });
     res.status(200).json(categoryData);
-  } catch {
+  } catch (err) {
     res.status(500).json(err);
   }
 }
