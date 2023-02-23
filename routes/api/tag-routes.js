a80da3ad45db6ac8d500d37c91dbebed9a7fb462
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-23 22:01:51 +0800

    updated readme

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all tags
   try {
     const tagData = await Tag.findAll({
-      include: [ { model: Product, Category, ProductTag } ],
+      include: [ {model: Product} ],
     });
     res.status(200).json(tagData)
   } catch (err) {
@@ -20,7 +20,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [ { model: Product, Category, ProductTag } ],
+      include: [ { model: Product } ],
     })
 
     if (!tagData) {
